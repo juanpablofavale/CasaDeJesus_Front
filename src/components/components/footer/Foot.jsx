e457b82eaa336d97d28d4d2a5f87f20a36c0184d
jpf <juanpablofavale@hotmail.com>
2024-05-01 19:35:22 -0300

    dando forma al footer

--- src/components/components/footer/Foot.jsx
+++ src/components/components/footer/Foot.jsx
@@ -3,7 +3,26 @@ import './Foot.css'
 const Foot = () => {
   return (
     <footer>
-      <div>Footer</div>
+      <div className='content'>
+        <div className='nav-foot'>
+          <div className='logos'>
+            <img src="/img/Logo2.png" alt="logo" />
+            <img src="/img/Logo3.png" alt="logo" />
+          </div>
+          <nav>
+            <a href="">Inicio</a>
+            <a href="">Acerca de</a>
+            <a href="">Obras</a>
+            <a href="">Contacto</a>
+          </nav>
+        </div>
+        <div className='redes'>
+          <a href=""><img src="/img/facebook-icon.png" alt="" /></a>
+          <a href=""><img src="/img/instagram-icon.png" alt="" /></a>
+          <a href=""><img src="/img/twitter-icon.png" alt="" /></a>
+          <a href=""><img src="/img/tiktok-icon.png" alt="" /></a>
+        </div>
+      </div>
     </footer>
   )
 }
