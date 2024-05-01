aecf4d607227a715ec423ae44e953329b19a7e37
jpf <juanpablofavale@hotmail.com>
2024-05-01 19:47:19 -0300

    mas estilos al footer

--- src/components/components/footer/Foot.jsx
+++ src/components/components/footer/Foot.jsx
@@ -3,24 +3,22 @@ import './Foot.css'
 const Foot = () => {
   return (
     <footer>
-      <div className='content'>
-        <div className='nav-foot'>
-          <div className='logos'>
-            <img src="/img/Logo2.png" alt="logo" />
-            <img src="/img/Logo3.png" alt="logo" />
-          </div>
-          <nav>
-            <a href="">Inicio</a>
-            <a href="">Acerca de</a>
-            <a href="">Obras</a>
-            <a href="">Contacto</a>
-          </nav>
+      <div className='nav-foot container'>
+        <div className='logos'>
+          <img src="/img/Logo2.png" alt="logo" />
+          <img src="/img/Logo3.png" alt="logo" />
         </div>
+        <nav>
+          <a href="">Inicio</a>
+          <a href="">Acerca de</a>
+          <a href="">Obras</a>
+          <a href="">Contacto</a>
+        </nav>
         <div className='redes'>
-          <a href=""><img src="/img/facebook-icon.png" alt="" /></a>
-          <a href=""><img src="/img/instagram-icon.png" alt="" /></a>
-          <a href=""><img src="/img/twitter-icon.png" alt="" /></a>
-          <a href=""><img src="/img/tiktok-icon.png" alt="" /></a>
+          <a href=""><img className='red' src="/img/facebook-icon.png" alt="facebook" /></a>
+          <a href=""><img className='red' src="/img/instagram-icon.png" alt="instagram" /></a>
+          <a href=""><img className='red' src="/img/twitter-icon.png" alt="twitter" /></a>
+          <a href=""><img className='red' src="/img/tiktok-icon.png" alt="tiktok" /></a>
         </div>
       </div>
     </footer>
