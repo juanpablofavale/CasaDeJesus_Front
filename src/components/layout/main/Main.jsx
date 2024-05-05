b7034d7059aa166d4e89cab3745874c294745a93
jpf <juanpablofavale@hotmail.com>
2024-05-05 16:19:58 -0300

    solucionando problema de links

--- src/components/layout/main/Main.jsx
+++ src/components/layout/main/Main.jsx
@@ -1,9 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Foot from '../../components/footer/Foot.jsx'
 import Head from '../../components/header/Head.jsx'
 import './Main.css'
+import { useEffect } from 'react'
 
 const Main = () => {
+
+  let {pathname} = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0,0)
+  }, [pathname]);
+
   return (
     <>
       <Head/>
